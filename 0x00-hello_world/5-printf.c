f41f38bcb019425bcee6e5f2c87ecf838062b3f2
sup3rUman <eliaskyalo.ng@gmail.com>
2023-06-15 00:13:37 -0700

    c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*@void - null
+*Return: Always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+
+	return (0);
+
+}
